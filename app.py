f239d05e2993fe508390555aec035f4308538c25
Nathan Douieb <Douiebnathan1999@gmail.com>
2025-03-06 18:04:19 +0200

    Adding form

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify, render_template, url_for
+from flask_cors import CORS
 import numpy as np
 import pandas as pd
 import pickle
 
 app = Flask(__name__)
+CORS(app)
+
 model = pickle.load(open('model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
 
@@ -19,9 +22,16 @@ def predict():
     prediction = model.predict(data_scaled)
     return jsonify({'prediction': prediction[0]})
 
+@app.route('/predict_user', methods = ['POST'])
+def user_predict():
+    data = [float(x) for x in request.form.values()]
+    columns_name = list(request.form.keys())
 
-
-
+    dataframe = pd.DataFrame([data], columns = columns_name)
+    final_input = scaler.transform(dataframe)
+    prediction = model.predict(final_input)[0]
+    # prediction_text doit être identique dans html
+    return render_template('home.html', prediction_text = f'The predicted price is: {prediction}')
 
 
 if __name__ == '__main__':
